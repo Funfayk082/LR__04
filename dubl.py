9950a84f64a36a748c8ae5d70fda305fa6f8871c
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2022-01-31 20:00:25 +0300

    Update dubl.py

--- dubl.py
+++ dubl.py
@@ -9,7 +9,17 @@ def dictionary():
 
 
 def duplicate():
-  pass
+  dub = {}
+    for i1 in t:
+        for i2 in t:
+            if i1 == i2:
+                pass
+            elif i1[i1.rfind('\\'):] == i2[i2.rfind('\\'):] and t[i1] == t[i2]:
+                if i1 in dub:
+                    continue
+                dub.update({i1: t[i1]})
+    else:
+        return dub
 
 
 def duplicate2():
